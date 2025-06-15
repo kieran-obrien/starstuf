686fbe198c223f743fcb44181fb6178857293f51
Kieran O’Brien <hello@kieranobrien.dev>
2025-06-15 22:46:23 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-// vite.config.js
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/starstuf/",
+  build: {
+    target: "esnext",
+  },
   esbuild: {
     target: "esnext",
   },
   server: {
-    host: "0.0.0.0", // allows access from other devices on the network
+    host: "0.0.0.0",
   },
 });
